b718649a499ed55cba34df0de8ae620ca5fff035
gregmarquet <marquet.greg@gmail.com>
2018-09-27 13:58:44 -0400

    add displayValue state and inputDigit state handler function in Calculator

--- src/components/Calculator/Calculator.js
+++ src/components/Calculator/Calculator.js
@@ -5,11 +5,25 @@ import './Calculator.css';
 class Calculator extends Component {
   constructor(props) {
     super(props);
+    this.state = {
+      displayValue: '0'
+    }
   }
+
+  inputDigit(digit) {
+    const { displayValue } = this.state;
+
+    this.setState({
+      displayValue: String(digit)
+    })
+  }
+
   render() {
+    const { displayValue } = this.state
+
     return (
       <div className="calculator">
-        <div className="calculator-display">0</div>
+        <div className="calculator-display">{displayValue}</div>
         <div className="calculator-keypad">
           <div className="input-keys">
             <div className="function-keys">
@@ -18,17 +32,28 @@ class Calculator extends Component {
               <button className="calculator-key key-percent">%</button>
             </div>
             <div className="digit-keys">
-              <button className="calculator-key key-0">0</button>
-              <button className="calculator-key key-dot">●</button>
-              <button className="calculator-key key-1">1</button>
-              <button className="calculator-key key-2">2</button>
-              <button className="calculator-key key-3">3</button>
-              <button className="calculator-key key-4">4</button>
-              <button className="calculator-key key-5">5</button>
-              <button className="calculator-key key-6">6</button>
-              <button className="calculator-key key-7">7</button>
-              <button className="calculator-key key-8">8</button>
-              <button className="calculator-key key-9">9</button>
+              <button className="calculator-key key-0"
+                onClick={() => this.inputDigit(0)}>0</button>
+              <button className="calculator-key key-dot"
+                onClick={() => this.inputDigit(1)}>●</button>
+              <button className="calculator-key key-1"
+                onClick={() => this.inputDigit(1)}>1</button>
+              <button className="calculator-key key-2"
+                onClick={() => this.inputDigit(2)}>2</button>
+              <button className="calculator-key key-3"
+                onClick={() => this.inputDigit(3)}>3</button>
+              <button className="calculator-key key-4"
+                onClick={() => this.inputDigit(4)}>4</button>
+              <button className="calculator-key key-5"
+                onClick={() => this.inputDigit(5)}>5</button>
+              <button className="calculator-key key-6"
+                onClick={() => this.inputDigit(6)}>6</button>
+              <button className="calculator-key key-7"
+                onClick={() => this.inputDigit(7)}>7</button>
+              <button className="calculator-key key-8"
+                onClick={() => this.inputDigit(8)}>8</button>
+              <button className="calculator-key key-9"
+                onClick={() => this.inputDigit(9)}>9</button>
             </div>
           </div>
           <div className="operator-keys">
